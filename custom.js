645f826bc087eafb1d8041c6b4ea8687a28e90c7
carmelo957 <102051077+carmelo957@users.noreply.github.com>
2022-03-21 11:19:11 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-window.snake.more_menu = function() {
+window.snake.more_menu = function() window.snake.more_menu();{
   window.snake_scheme_epic_cool = window.snake_scheme_epic_cool || { light_squares: '#AAD751', dark_squares:  '#A2D149' };
   window.snake_scheme_epic_cool = window.snake_scheme_epic_cool || { light_squares: '#AAD751', dark_squares:  '#A2D149' };
   
